ab224e0b96a9dc76218930ee092fd7e045b4f042
vickyone0 <vigneshs0.618@gmail.com>
2025-04-30 01:44:57 +0530

    inserction sort

--- main.rs
+++ main.rs
@@ -2,25 +2,21 @@ fn main() {
 
     let mut arr = [5, 2, 9, 1, 5, 6];
     println!("Before sorting: {:?}", arr);
-    bubble_sort(&mut arr);
+    insertion_sort(&mut arr);
     println!("After sorting: {:?}", arr);
 
 }
 
-fn bubble_sort(arr: &mut [i32]) {
-
-    for i in 0..(arr.len()-1) {
-        let mut swapped = false;
-
-        for j in 0..(arr.len()-1-i) {
-            if arr[j] > arr[j+1] {
-                arr.swap(j, j+1);
-                swapped = true;
-            }
+pub fn insertion_sort<T: Ord>(arr: &mut [T]) {
+    // Start from the second element (index 1)
+    for i in 1..arr.len() {
+        let mut j = i;
+        
+        // Move elements of arr[0..i-1] that are greater than arr[i]
+        // to one position ahead of their current position
+        while j > 0 && arr[j] < arr[j - 1] {
+            arr.swap(j, j - 1);
+            j -= 1;
         }
-        if !swapped {
-            break;
-        }
-
     }
 }
